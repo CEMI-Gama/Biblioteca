7ed69f66c75ffe280e3bc6fdad3440930aa1c625
oo7gabriel <gabrielfsacademico@gmail.com>
2019-05-14 09:48:38 -0300

    Administrator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Gabriel', class_code: '3D', register: '000337751', cpf: '70843816198', address: 'sul', level: 0, password:'1234', password_confirmation: '1234')
+User.create(name: 'Chris', class_code: '3D', register: '498053039', cpf: '70852532156', address: 'sul', level: 1, password:'1234', password_confirmation: '1234')
+Book.create(title: '1984', author: 'George Orwell', publishing_company: 'Companhia das Letras', year: '2018', category: 'Romance', bar_code: '9788535914849', source_of_donation: 'Christyan', amount: '1')

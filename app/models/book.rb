698bc7699b46239e5700289f6363e003abbec4c1
VinyPinheiro <viny-pinheiro@hotmail.com>
2019-06-20 12:58:11 -0300

    Generate automatic barcode

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  before_save do |doc|
+    doc.bar_code = doc.generate_bar_code
+  end
+
   validates :title, presence: { message: 'Preencha o campo' }
   validates :author, presence: { message: 'Preencha o campo' }
   validates :publishing_company, presence: { message: 'Preencha o campo' }
@@ -8,4 +12,12 @@ class Book < ApplicationRecord
   validates :bar_code, uniqueness: { message: 'Insira outro código de barra' }
   validates :source_of_donation, presence: { message: 'Preencha o campo' }
   validates :amount, presence: { message: 'Preencha o campo' }
+
+  def generate_bar_code
+    require 'digest'
+    loop do
+      bar_code = [*('0'..'9')].sample(13).join
+      break bar_code if Book.find_by(bar_code: bar_code).nil?
+    end
+  end
 end
